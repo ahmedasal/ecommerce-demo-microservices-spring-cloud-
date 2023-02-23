6cf07a1066586758bb4251ed808d7b3ed47e85e6
ahmed <ahmed.asal510@gmail.com>
2023-02-23 19:23:25 +0200

    - add keycloak as authorization server - add registering for customer and Merchant - provide security for apis service - add gateway service - add discovery-service

--- services/system/gateway/src/main/java/demos/ahmed/gateway/GatewayApplication.java
+++ services/system/gateway/src/main/java/demos/ahmed/gateway/GatewayApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreake
 import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerResilience4JFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,30 +22,11 @@ import reactor.core.publisher.Mono;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 @RequestMapping
 public class GatewayApplication {
-//    @Bean
-//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(p -> p
-//                        .path("/get")
-//                        .filters(f -> f.addRequestHeader("Hello", "World"))
-//                        .uri("http://httpbin.org:80"))
-//                .route(p -> p
-//                        .host("*.circuitbreaker.com")
-//                        .filters(f -> f.circuitBreaker(config -> config
-//                                .setName("mycmd")
-//                                .setFallbackUri("forward:/fallback")))
-//                        .uri("http://httpbin.org:80"))
-//                .build();
-//    }
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @RequestMapping("/fallback")
-    public Mono<String> fallback() {
-        return Mono.just("fallback");
     }
 }
